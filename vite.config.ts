c85ffc6aa7d9e94a86472503f3b86e128fa8e310
nxtime <marcos.paulodasilva.mp@gmail.com>
2023-08-22 20:16:46 -0300

    fix: lib export

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig } from "vite";
-import { resolve } from 'path'
+import { resolve } from "path";
 import react from "@vitejs/plugin-react";
+import tsConfigPaths from "vite-tsconfig-paths";
+import dts from "vite-plugin-dts";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsConfigPaths(), dts()],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "DesignSustem",
+      formats: ['es', "umd"],
       fileName: "design-system",
     },
     rollupOptions: {
